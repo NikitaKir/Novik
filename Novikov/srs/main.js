07000b64c2165d19dcd2765b8f354c720393b8b5
NikitaKir <103379081+NikitaKir@users.noreply.github.com>
2022-06-12 19:16:43 +0400

    Add files via upload

---
+++ Novikov/srs/main.js
@@ -0,0 +1,41 @@
+(function($){
+  $(window).on("load",function(){
+      $("a[rel='m_PageScroll2id']").mPageScroll2id({
+        layout:"auto"
+      });
+     
+  });
+})(jQuery);
+
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("prewieSlide");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
